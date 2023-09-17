e94f58c7015093a3edc99b8f54845b6ff7dd5103
givehug <vboretskyi@gmail.com>
2023-09-17 15:19:53 +0200

    2015 - 24 done bf

--- 2015/24/bun.ts
+++ 2015/24/bun.ts
@@ -1,51 +1,103 @@
-const calcQuantumEntanglement = (arr: number[]) => {
+const logger = (() => {
+  let count = 1;
+
+  const log = (...args: any[]) => {
+    console.log(count++, ...args);
+  };
+
+  return {
+    log,
+  };
+})();
+
+const calcProduct = (arr: number[]) => {
   return arr.reduce((a, b) => a * b);
 };
 
-const calcLoad = (arr: number[]) => {
+const calcSum = (arr: number[]) => {
+  if (!arr.length) {
+    return 0;
+  }
   return arr.reduce((a, b) => a + b);
 };
 
-const isLoadEqual = (...compartments: number[][]) => {
-  return compartments.every(
-    (cpt) => calcLoad(cpt) === calcLoad(compartments[0])
-  );
+const calcCompartmentLoad = (weights: number[], compartments: number) => {
+  return calcSum(weights) / compartments;
 };
 
-const distributeLoad = (weights: number[]) => {
-  const p1: number[] = [];
-  const p2: number[] = [];
-  const p3: number[] = [];
-  const history: any = [];
+const findLoadCombos = (weights: number[], pLoad: number) => {
+  const combos = new Map<string, number>();
+  // const limit = 20;
+  let shortestCombo = Infinity;
+  let lastAddedAt = Date.now();
+  let abortedAfter5Seconds = false;
 
-  // demo 1
-  // p1.push(11, 9);
-  // p2.push(10, 8, 2);
-  // p3.push(7, 5, 4, 3, 1);
+  const findCombos = (_weights: number[], combo: number[] = []) => {
+    if (combo.length > shortestCombo) {
+      return;
+    }
 
-  // demo 2
-  // p1.push(10, 9, 1);
-  // p2.push(11, 7, 2);
-  // p3.push(8, 5, 4, 3);
+    if (Date.now() - lastAddedAt > 5000) {
+      abortedAfter5Seconds = true;
+      return;
+    }
 
-  // while (true) {
-  if (isLoadEqual(p1, p2, p3)) {
-    history.push({ p1, p2, p3, qe: calcQuantumEntanglement(p1) });
+    const load = calcSum(combo);
+
+    if (load > pLoad) {
+      return;
+    }
+
+    if (load === pLoad) {
+      combo.sort((a, b) => b - a);
+      const comboHash = combo.join("-");
+      const eq = calcProduct(combo); // TODO: track lowest QE / combo length
+      if (!combos.has(comboHash)) {
+        shortestCombo = Math.min(shortestCombo, combo.length);
+        combos.set(comboHash, eq);
+        lastAddedAt = Date.now();
+      }
+      return;
+    }
+
+    for (let i = 0; i < _weights.length; i++) {
+      const newCombo = [...combo, _weights[i]];
+      const newWeights = _weights.filter((_, j) => j !== i);
+      findCombos(newWeights, newCombo);
+    }
+  };
+
+  findCombos(weights);
+  if (abortedAfter5Seconds) {
+    logger.log("Couldn't find more combos for 5 seconds, aborting...");
   }
-  // }
+  logger.log("Found load combos:", combos.size);
 
-  return history;
+  return combos;
 };
 
+const distributeLoad = (weights: number[], compartments: number) => {
+  const pLoad = calcCompartmentLoad(weights, compartments);
+  logger.log("Compartment load:", pLoad);
+
+  const combos = findLoadCombos(weights, pLoad);
+  return combos;
+};
+
+// Dummy BF
 async function main() {
   const file = Bun.file("input.txt");
   const input = await file.text();
   const lines = input.split("\n");
-  const weights = lines.map(Number);
 
-  const res = distributeLoad(weights);
+  const weights = lines.map(Number).sort((a, b) => b - a);
+  logger.log("Weights:", weights);
 
-  console.log(res);
+  const combosMap = distributeLoad(weights, 4);
+
+  const res = Array.from(combosMap).sort((a, b) => a[1] - b[1]);
+
+  console.log(res.slice(0, 5));
 }
 
 main();
